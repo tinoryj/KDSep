5a52394135784dd295012ed6fe48dfa7cd831a86
tinoryj <tinoryj@gmail.com>
2022-10-22 00:40:52 +0800

    Update test scripts for RocksDB RMW and Merge Updadte

--- Simulator/YCSB-C/scripts/runUpdateTest.sh
+++ Simulator/YCSB-C/scripts/runUpdateTest.sh
@@ -26,3 +26,25 @@ do
         ./ycsbc -db rocksdb -dbfilename dbtest -threads 16 -P workloada-temp.spec  -phase load  -configpath configDir/leveldb_config.ini > ./ResultLogs/Read-0.$i-Update-0.$UpdateProportion-Round-$multiRun.log
     done
 done
+
+for i in {1..9}
+do 
+    for multiRun in {1..10}
+    do
+        let RMWProportion=10-i
+        echo "Modify spec, Read/RMW ratio = $i:$RMWProportion"
+        cp workloads/workloada-test-RMW.spec ./workloada-temp.spec
+        sed -i "15s/0.5/0.$i/g" workloada-temp.spec
+        sed -i "17s/0.5/0.$RMWProportion/g" workloada-temp.spec
+        echo "Modified spec file content =======>"
+        cat workloada-temp.spec | head -n 17 | tail -n 3
+
+        # Running the ycsb-benchmark
+        make clean
+        make
+        echo "Loading the database =====================>"
+        ./ycsbc -db rocksdb -dbfilename dbtest -threads 16 -P workloada-temp.spec  -phase load  -configpath configDir/leveldb_config.ini
+        echo "Benchmark the database =====================>"
+        ./ycsbc -db rocksdb -dbfilename dbtest -threads 16 -P workloada-temp.spec  -phase load  -configpath configDir/leveldb_config.ini > ./ResultLogs/Read-0.$i-RMW-0.$RMWProportion-Round-$multiRun.log
+    done
+done
