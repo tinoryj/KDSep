86fe2ce54f7450b34d40c96f2ed948900ee4dea3
tinoryj <tinoryj@gmail.com>
2022-10-26 14:00:57 +0800

    update temp

--- Simulator/YCSB-C/scripts/runUpdateTest.sh
+++ Simulator/YCSB-C/scripts/runUpdateTest.sh
@@ -3,15 +3,20 @@ pwd
 ReadRatioSet=(0.1 0.45 0.8)
 OverWriteRatio=0.1
 ResultLogFolder="ResultLogs"
-DB_Name="dbtest"
-Thread_number=1
-KVPairsNumber=5000    #"50000000"
-OperationsNumber=5000 #"50000000"
+DB_Name="loadedDB"
+DB_Loaded_Path="/mnt/sn640"
+KVPairsNumber=300000000    #"50000000"
+OperationsNumber=300000000 #"50000000"
 MAXRunTimes=1
+Thread_number=1
 
 if [ ! -d $ResultLogFolder ]; then
     mkdir -p $ResultLogFolder
 fi
+
+echo "<===================== Loading the database =====================>"
+./ycsbc -db rocksdb -dbfilename $DB_Name -threads $Thread_number -P workloada-temp.spec -phase load -configpath configDir/leveldb_config.ini
+cp -r $DB_Name $DB_Loaded_Path/ # Copy loaded DB
 
 for ((roundIndex = 1; roundIndex <= $MAXRunTimes; roundIndex++)); do
 
@@ -37,13 +42,18 @@ for ((roundIndex = 1; roundIndex <= $MAXRunTimes; roundIndex++)); do
         if [ -d $DB_Name ]; then
             rm -rf $DB_Name
             echo "Deleted old database folder"
+            cp -r $DB_Loaded_Path/$DB_Name ./
+            echo "Copy loaded database"
         fi
-        echo "<===================== Loading the database (Round $roundIndex) =====================>"
-        ./ycsbc -db rocksdb -dbfilename $DB_Name -threads $Thread_number -P workloada-temp.spec -phase load -configpath configDir/leveldb_config.ini
-        exit
-        echo "<===================== Benchmark the database (Round $roundIndex) =====================>"
+        if [ ! -d $DB_Name ]; then
+            echo "Retrived loaded database error"
+            exit
+        fi
+
+        echo "<===================== Benchmark the database (Round $roundIndex) start =====================>"
         ./ycsbc -db rocksdb -dbfilename $DB_Name -threads $Thread_number -P workloada-temp.spec -phase run -configpath configDir/leveldb_config.ini >$ResultLogFolder/Read-$ReadProportion-Update-0$UpdateProportion-OverWrite-$OverWriteRatio-Round-$roundIndex.log
         echo "<===================== Benchmark the database (Round $roundIndex) done =====================>"
+        mv $DB_Name $DB_Loaded_Path/Read-$ReadProportion-Update-0$UpdateProportion-OverWrite-$OverWriteRatio-DB
 
         # Running RMW
 
@@ -66,10 +76,15 @@ for ((roundIndex = 1; roundIndex <= $MAXRunTimes; roundIndex++)); do
         if [ -d $DB_Name ]; then
             rm -rf $DB_Name
             echo "Deleted old database folder"
+            cp -r $DB_Loaded_Path/$DB_Name ./
+            echo "Copy loaded database"
         fi
-        echo "<===================== Loading the database (Round $roundIndex) =====================>"
-        ./ycsbc -db rocksdb -dbfilename $DB_Name -threads $Thread_number -P workloada-temp.spec -phase load -configpath configDir/leveldb_config.ini
-        echo "<===================== Benchmark the database (Round $roundIndex) =====================>"
+        if [ ! -d $DB_Name ]; then
+            echo "Retrived loaded database error"
+            exit
+        fi
+
+        echo "<===================== Benchmark the database (Round $roundIndex) start =====================>"
         ./ycsbc -db rocksdb -dbfilename $DB_Name -threads $Thread_number -P workloada-temp.spec -phase run -configpath configDir/leveldb_config.ini >$ResultLogFolder/Read-$ReadProportion-RMW-0$RMWProportion-OverWrite-$OverWriteRatio-Round-$roundIndex.log
         echo "<===================== Benchmark the database (Round $roundIndex) done =====================>"
 
