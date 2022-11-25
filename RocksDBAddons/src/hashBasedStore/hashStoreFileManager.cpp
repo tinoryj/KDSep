1f9d1cb8ff6433daa6962a3b467dd35b5f7542cd
tinoryj <tinoryj@gmail.com>
2022-11-26 01:15:26 +0800

    Fixed file create with fstream

--- RocksDBAddons/src/hashBasedStore/hashStoreFileManager.cpp
+++ RocksDBAddons/src/hashBasedStore/hashStoreFileManager.cpp
@@ -63,6 +63,8 @@ bool HashStoreFileManager::RetriveHashStoreFileMetaDataList()
             currentFileHandlerPtr->total_object_bytes_ = currentFileStoredBytes;
             // open current file for further usage
             currentFileHandlerPtr->fileOperationMutex_.lock();
+            currentFileHandlerPtr->file_operation_stream_.open(workingDir_ + to_string(currentFileHandlerPtr->target_file_id_), ios::out | ios::binary);
+            currentFileHandlerPtr->file_operation_stream_.close();
             currentFileHandlerPtr->file_operation_stream_.open(workingDir_ + to_string(currentFileHandlerPtr->target_file_id_), ios::in | ios::out | ios::binary);
             currentFileHandlerPtr->fileOperationMutex_.unlock();
             // re-insert into trie and map for build index
@@ -236,10 +238,13 @@ bool HashStoreFileManager::createAndGetNewHashStoreFileHandlerByPrefix(const str
     // write header to current file
     currentFileHandlerPtr->fileOperationMutex_.lock();
     cout << BLUE << "[DEBUG-LOG]:[Addons]-[HashStoreFileManager]-[createAndGetNewHashStoreFileHandlerByPrefix] in lock, file name = " << workingDir_ + "/" + to_string(currentFileHandlerPtr->target_file_id_) + ".delta" << RESET << endl;
+    currentFileHandlerPtr->file_operation_stream_.open(workingDir_ + "/" + to_string(currentFileHandlerPtr->target_file_id_) + ".delta", ios::out | ios::binary);
+    currentFileHandlerPtr->file_operation_stream_.close();
     currentFileHandlerPtr->file_operation_stream_.open(workingDir_ + "/" + to_string(currentFileHandlerPtr->target_file_id_) + ".delta", ios::in | ios::out | ios::binary);
     cout << BLUE << "[DEBUG-LOG]:[Addons]-[HashStoreFileManager]-[createAndGetNewHashStoreFileHandlerByPrefix] current file read pointer = " << currentFileHandlerPtr->file_operation_stream_.tellg() << ", current file write pointer = " << currentFileHandlerPtr->file_operation_stream_.tellp() << RESET << endl;
     currentFileHandlerPtr->file_operation_stream_.write(fileHeaderWriteBuffer, sizeof(newFileHeader));
     cout << BLUE << "[DEBUG-LOG]:[Addons]-[HashStoreFileManager]-[createAndGetNewHashStoreFileHandlerByPrefix] after write current file read pointer = " << currentFileHandlerPtr->file_operation_stream_.tellg() << ", current file write pointer = " << currentFileHandlerPtr->file_operation_stream_.tellp() << RESET << endl;
+    currentFileHandlerPtr->total_object_bytes_ += sizeof(newFileHeader);
     currentFileHandlerPtr->fileOperationMutex_.unlock();
     // move pointer for return
     objectFileMetaDataTrie_.insert(prefixStr.substr(0, initialTrieBitNumber_), currentFileHandlerPtr);
